3905387ae0d59bb4fd8c2b7da195c902a58cb953
Gary Jackson <garyljackson@gmail.com>
2020-03-02 21:15:52 +1000

    Add models for client

---
+++ ConsoleApp/Models/BookPostRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp.Models
+{
+    public class BookPostRequest
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+    }
+}
