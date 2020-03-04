15aeec81b21fb16a67a124087f6693ddb38bbfe9
Gary Jackson <garyljackson@gmail.com>
2020-03-04 21:30:19 +1000

    Refactored test 7

--- ConsoleApp/Tests/TestHttpClient7.cs
+++ ConsoleApp/Tests/TestHttpClient7.cs
@@ -14,68 +14,83 @@ using System.Threading.Tasks;
         public TestHttpClient7(HttpClient httpClient)
         {
             _httpClient = httpClient;
-
-            // This needs to be the root of the url, I tested it by including /api/books/ and that didn't work
-            // Could also be configured using services.AddHttpClient
             _httpClient.BaseAddress = new Uri("https://localhost:44342/"); 
         }
 
         public async Task<T> GetAsync<T>(Uri requestUri)
         {
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
-            using var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead);
-            await using var contentStream = await responseMessage.Content.ReadAsStreamAsync();
-            return await JsonSerializer.DeserializeAsync<T>(contentStream, DefaultJsonSerializerOptions.Options);
+            using var requestMessage = CreateRequestMessage(HttpMethod.Get, requestUri);
+            using var responseMessage = await SendAsync(requestMessage);
+
+            return await DeserializeAsync<T>(responseMessage.Content);
         }
         
-        public async Task<TResponse> PostAsync<TRequest, TResponse>(Uri requestUri, TRequest requestBody)
+        public async Task<T> PostAsync<TRequest, T>(Uri requestUri, TRequest requestBody)
         {
             await using var requestStream = await SerializeAsync(requestBody);
+            using var streamContent = CreateStreamContent(requestStream);
+            using var requestMessage = CreateRequestMessage(HttpMethod.Post, requestUri, streamContent);
+            using var responseMessage = await SendAsync(requestMessage);
 
-            using var streamContent = GetContent(requestStream);
-
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, requestUri) {Content = streamContent};
-
-            using var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead);
-            responseMessage.EnsureSuccessStatusCode();
-
-            await using var contentStream = await responseMessage.Content.ReadAsStreamAsync();
-            return await JsonSerializer.DeserializeAsync<TResponse>(contentStream, DefaultJsonSerializerOptions.Options);
+            return await DeserializeAsync<T>(responseMessage.Content);
         }
 
         public async Task PutAsync<TRequest>(Uri requestUri, TRequest requestBody)
         {
             await using var requestStream = await SerializeAsync(requestBody);
-
-            using var streamContent = GetContent(requestStream);
-
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Put, requestUri) {Content = streamContent};
-
-            using var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead);
-            responseMessage.EnsureSuccessStatusCode();
+            using var streamContent = CreateStreamContent(requestStream);
+            using var requestMessage = CreateRequestMessage(HttpMethod.Put, requestUri, streamContent);
+            using var responseMessage = await SendAsync(requestMessage);
         }
 
         public async Task<T> DeleteAsync<T>(Uri requestUri)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Delete, requestUri);
-            using var responseMessage = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            using var requestMessage = CreateRequestMessage(HttpMethod.Delete, requestUri);
+            using var responseMessage = await SendAsync(requestMessage);
             await using var contentStream = await responseMessage.Content.ReadAsStreamAsync();
-            return await JsonSerializer.DeserializeAsync<T>(contentStream, DefaultJsonSerializerOptions.Options);
+
+            return await DeserializeAsync<T>(responseMessage.Content);
         }
 
-        private async Task<Stream> SerializeAsync<T>(T requestBody)
+        private static async Task<Stream> SerializeAsync<T>(T requestBody)
         {
             var stream = new MemoryStream();
             await JsonSerializer.SerializeAsync(stream, requestBody, DefaultJsonSerializerOptions.Options);
             stream.Seek(0, SeekOrigin.Begin);
+
             return stream;
         }
 
-        private StreamContent GetContent(Stream stream)
+        private static async Task<T> DeserializeAsync<T>(HttpContent httpContent)
+        {
+            await using var contentStream = await httpContent.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<T>(contentStream, DefaultJsonSerializerOptions.Options);
+        }
+
+        private static StreamContent CreateStreamContent(Stream stream)
         {
             var streamContent = new StreamContent(stream);
             streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             return streamContent;
+        }
+
+        private static HttpRequestMessage CreateRequestMessage(HttpMethod method, Uri requestUri, HttpContent streamContent)
+        {
+            var requestMessage = new HttpRequestMessage(method, requestUri) {Content = streamContent};
+            return requestMessage;
+        }
+
+        private static HttpRequestMessage CreateRequestMessage(HttpMethod method, Uri requestUri)
+        {
+            var requestMessage = new HttpRequestMessage(method, requestUri);
+            return requestMessage;
+        }
+
+        private async Task<HttpResponseMessage> SendAsync(HttpRequestMessage requestMessage)
+        {
+            var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead);
+            responseMessage.EnsureSuccessStatusCode();
+            return responseMessage;
         }
     }
 }
